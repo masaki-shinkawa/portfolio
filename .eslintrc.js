8e663b779eb86734eb84ac0fee187a779895340f
Masaki.Shinkawa <masaki.shinkawa@adglobe.co.jp>
2020-12-30 18:07:14 +0900

    feat: add nuxt

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
-};
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {},
+}
--- src/consts/route.js
+++
@@ -1,6 +0,0 @@
-export const routeItems = [
-  { title: "HOME", icon: "home", path: "/" },
-  { title: "PROFILE", icon: "account_circle", path: "/profile" },
-  { title: "PRODUCTION", icon: "dashboard", path: "/production" },
-  { title: "BLOG", icon: "message", path: "/blog" }
-];
